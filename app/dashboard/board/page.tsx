e09b2db8797e8d12a60edc3e3c9c6699799a654a
Abubacar Correia <abubacar.paulo.correia@gmail.com>
2024-02-27 23:10:31 +0100

    feat: adding board page

--- app/dashboard/board/page.tsx
+++ app/dashboard/board/page.tsx
@@ -1,4 +1,21 @@
-export default function Page(){
+import ActionsElements from "@/app/ui/board/actions-elements";
+import { ColumnCards } from "@/app/ui/board/cards";
+import TopBar from "@/app/ui/board/topBar";
+import Filter from "@/app/ui/filter";
+import { UserAvatares } from "@/app/ui/users-avatares";
 
-  return (<h1>BOARD</h1>)
+export default function Page() {
+  return (
+    <main className="w-full h-screen p-2 md:p-4 space-y-4 overflow-hidden">
+      <TopBar />
+      <UserAvatares />
+      <div className="flex items-center justify-between">
+        <Filter />
+        <ActionsElements />
+      </div>
+      <div className="grid grid-cols-[repeat(5,minmax(300px,_1fr))] overflow-scroll w-full pb-5">
+        <ColumnCards />
+      </div>
+    </main>
+  );
 }
