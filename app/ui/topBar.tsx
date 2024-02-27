7e62af500f2f88b9330b7b405aaaa6d3811a7a68
Abubacar Correia <abubacar.paulo.correia@gmail.com>
2024-02-27 23:08:04 +0100

    fix: updating top on dashboard page

--- app/ui/topBar.tsx
+++ app/ui/topBar.tsx
@@ -3,7 +3,7 @@ import { Plus, Presentation } from "lucide-react";
 export default function TopBar() {
   return (
     <div className="flex justify-end md:justify-between items-center">
-      <p className="text-sm font-bold hidden md:block">Painel</p>
+      <h3 className="text-sm font-bold hidden md:block">Painel</h3>
       <div className="flex gap-2">
         <button className="flex gap-1 bg-gray-100 p-2 text-gray-800 text-sm rounded-md shadow-sm">
           <Plus className="size-5 text-gray-400"></Plus>
